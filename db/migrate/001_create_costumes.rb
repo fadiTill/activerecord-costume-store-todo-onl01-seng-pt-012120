4fcfd45d1669de364c294182034cc2a255c59312
fadimata tillman <fadi.tillman@gmail.com>
2020-04-16 18:39:06 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -5,7 +5,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
     t.string  :name
     t.integer :price
     t.string  :image_url
-    t.string    :size
+    t.string  :size
     t.timestamps
     end
   end
