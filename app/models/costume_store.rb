d159383492ca615dc21d7770b53c707f07f449b6
fadimata tillman <fadi.tillman@gmail.com>
2020-04-16 18:47:52 +0000

    Automatically backed up by Learn

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -3,8 +3,9 @@ class CreateCostumesStores < ActiveRecord::Migration[4.2]
   def change
     create_table :costume_stores do |t|
     t.string  :name
-    t.integer :price
-    t.string  :image_url
+    t.string :location
+    t.integer :costume_inventory
+    t.string  :num_of_employees
     t.string  :size
     t.timestamps
     end
