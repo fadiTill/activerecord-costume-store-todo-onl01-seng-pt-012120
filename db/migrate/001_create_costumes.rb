da95185db03bbad64f3928f347b1ee74bad2ec0d
fadimata tillman <fadi.tillman@gmail.com>
2020-04-16 17:44:26 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,6 +1,6 @@
 # Create a class and inherit from ActiveRecord::Migration
 class CreateCostumes
-endActiveRecord::Migration[4.2]
+ActiveRecord::Migration[4.2]
   def change
     create_table :dogs do |t|
       t.string :name
