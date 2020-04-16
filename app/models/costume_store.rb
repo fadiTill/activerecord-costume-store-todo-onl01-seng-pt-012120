845a331f67331f90de7910af71f3542f85bbdab3
fadimata tillman <fadi.tillman@gmail.com>
2020-04-16 18:45:39 +0000

    Automatically backed up by Learn

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,1 +1,12 @@
 # Create your CostumeStore class here
+class CreateCostumes < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costumes do |t|
+    t.string  :name
+    t.integer :price
+    t.string  :image_url
+    t.string  :size
+    t.timestamps
+    end
+  end
+end
